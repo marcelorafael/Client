94552abe0264a0ce3fca949017a700e82f098641
marcelorafael <marcelorafael23@hotmail.com>
2022-02-01 21:53:45 -0300

    feat: MenuFull created.

--- src/components/Menu/test.tsx
+++ src/components/Menu/test.tsx
@@ -1,4 +1,4 @@
-import { screen } from '@testing-library/react'
+import { fireEvent, screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
 
 import Menu from '.'
@@ -7,9 +7,37 @@ describe('<Menu />', () => {
   it('should render the menu', () => {
     renderWithTheme(<Menu />)
 
+    // Verificando se estes elementos estão no documento.
     expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument()
     expect(screen.getByLabelText(/search/i)).toBeInTheDocument()
     expect(screen.getByLabelText(/open shopping cart/i)).toBeInTheDocument()
     expect(screen.getByRole('img', { name: /won games/i })).toBeInTheDocument()
+  })
+
+  it('should render the open/close mobile menu', () => {
+    renderWithTheme(<Menu />)
+
+    // Selecionando o Menu pela sua fumção.
+    const fullMenuElement = screen.getByRole('navigation', { hidden: true })
+
+    // Selecionando um atributo do elemento(aria-hidden) e verificando seu valor
+    expect(fullMenuElement.getAttribute('aria-hidden')).toBe('true')
+
+    // Verificando um estilo do menu
+    expect(fullMenuElement).toHaveStyle({ opacity: 0 })
+
+    // Simulando o click no menu para este abrir-se
+    fireEvent.click(screen.getByLabelText(/open menu/i))
+
+    // Mudando valores para que o menu seja aberto.
+    expect(fullMenuElement.getAttribute('aria-hidden')).toBe('false')
+    expect(fullMenuElement).toHaveStyle({ opacity: 1 })
+
+    // Simulando o click no menu para este fehcar-se
+    fireEvent.click(screen.getByLabelText(/close menu/i))
+
+    // Mudando valores para que o menu seja fechado.
+    expect(fullMenuElement.getAttribute('aria-hidden')).toBe('true')
+    expect(fullMenuElement).toHaveStyle({ opacity: 0 })
   })
 })
