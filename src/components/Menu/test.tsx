11d55d4b4a0770111c38a8c47be502285547d14d
marcelorafael <marcelorafael23@hotmail.com>
2022-02-08 10:22:51 -0300

    feat: created rule to logged in or logged out in init menu front-end

--- src/components/Menu/test.tsx
+++ src/components/Menu/test.tsx
@@ -40,4 +40,21 @@ describe('<Menu />', () => {
     expect(fullMenuElement.getAttribute('aria-hidden')).toBe('true')
     expect(fullMenuElement).toHaveStyle({ opacity: 0 })
   })
+
+  it('should show register box when logged out', () => {
+    renderWithTheme(<Menu />)
+
+    expect(screen.getByText(/Log in now/i)).toBeInTheDocument()
+    expect(screen.getByText(/Sign up/i)).toBeInTheDocument()
+  })
+
+  it('should show wishlist and account when logged in', () => {
+    renderWithTheme(<Menu username="Marcelo" />)
+
+    expect(screen.getByText(/wishlist/i)).toBeInTheDocument()
+    expect(screen.getByText(/my account/i)).toBeInTheDocument()
+
+    expect(screen.queryByText(/Log in now/i)).not.toBeInTheDocument()
+    expect(screen.queryByText(/Sign up/i)).not.toBeInTheDocument()
+  })
 })
