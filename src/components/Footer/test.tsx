f512e6a91406980c29f02ff16d87acf757dabb99
marcelorafael <marcelorafael23@hotmail.com>
2022-02-23 08:45:34 -0300

    feat: Add features in Button and Footer component

--- src/components/Footer/test.tsx
+++ src/components/Footer/test.tsx
@@ -1,10 +1,25 @@
+import { screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
 
 import Footer from '.'
 
 describe('<Footer />', () => {
-  it('should render the heading', () => {
+  it('should render 4 colomns topics', () => {
     const { container } = renderWithTheme(<Footer />)
+
+    expect(
+      screen.getByRole('heading', { name: /Contact/i })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: /follow us/i })
+    ).toBeInTheDocument()
+
+    expect(screen.getByRole('heading', { name: /links/i })).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: /location/i })
+    ).toBeInTheDocument()
 
     expect(container.firstChild).toMatchSnapshot()
   })
